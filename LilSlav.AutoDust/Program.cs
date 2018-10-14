6555685474d7863c6e0e906781d7b6efc6b385d1
Stefan <janjicstefan163@gmail.com>
2018-10-14 22:08:55 +0200

    test2

--- LilSlav.AutoDust/Program.cs
+++ LilSlav.AutoDust/Program.cs
@@ -15,7 +15,7 @@ namespace LilSlav.AutoDust
 		private static Hero _myHero;
 		private static Player _player;
 		private const int DustRange = 1000;					/* maksimalna udaljenost za dust */
-		private const string ScriptVersion = "1.0.1.2";		// za ispis
+		private const string ScriptVersion = "1.0.2.3";		// za ispis
 
 		#endregion
 
@@ -76,9 +76,38 @@ namespace LilSlav.AutoDust
 			foreach (var enemy in listEnemy)
 			{
 				// dodati logiku
-				if (enemy.Modifiers.Any(
-					x => (x.Name == "modifier_clinkz_wind_walk")))
+				//if (enemy.Modifiers.Any(
+				//	x => (x.Name == "modifier_clinkz_wind_walk")))
+				//{
+				//	itemDust.UseAbility();
+				//	Utils.Sleep(250, "delay");
+				//}
 				{
+					if (enemy.Modifiers.Any(
+						x =>
+							(x.Name == "modifier_bounty_hunter_wind_walk" ||
+							 x.Name == "modifier_riki_permanent_invisibility" ||
+							 x.Name == "modifier_mirana_moonlight_shadow" || x.Name == "modifier_treant_natures_guise" ||
+							 x.Name == "modifier_weaver_shukuchi" ||
+							 x.Name == "modifier_broodmother_spin_web_invisible_applier" ||
+							 x.Name == "modifier_item_invisibility_edge_windwalk" || x.Name == "modifier_rune_invis" ||
+							 x.Name == "modifier_clinkz_wind_walk" || x.Name == "modifier_item_shadow_amulet_fade" ||
+							 x.Name == "modifier_bounty_hunter_track" || x.Name == "modifier_bloodseeker_thirst_vision" ||
+							 x.Name == "modifier_slardar_amplify_damage" || x.Name == "modifier_item_dustofappearance") ||
+							x.Name == "modifier_invoker_ghost_walk_enemy"))
+					{
+						itemDust.UseAbility();
+						Utils.Sleep(250, "delay");
+					}
+
+					if ((enemy.Name == ("npc_dota_hero_templar_assassin") || enemy.Name == ("npc_dota_hero_sand_king")) &&
+						(enemy.Health / enemy.MaximumHealth < 0.3))
+					{
+						itemDust.UseAbility();
+						Utils.Sleep(250, "delay");
+
+					}
+					if (enemy.Name != ("npc_dota_hero_nyx_assassin") || !enemy.Spellbook.Spell4.CanBeCasted()) continue;
 					itemDust.UseAbility();
 					Utils.Sleep(250, "delay");
 				}
