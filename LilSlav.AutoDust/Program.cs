4b316f300a2c74bf520e3e125e92dc35b6833dad
Stefan <janjicstefan163@gmail.com>
2018-10-14 20:10:48 +0200

    Dodate globalne var. i pomocne funkcije
    Globalne promenljive koje su nam potrebne
    Funkcije za ispis u boji u zavisnosti od uspesnosti

--- LilSlav.AutoDust/Program.cs
+++ LilSlav.AutoDust/Program.cs
@@ -8,8 +8,45 @@ namespace LilSlav.AutoDust
 {
 	class Program
 	{
+		#region globalne_varijable
+
+		private static bool _loaded;
+		private static Hero _myHero;
+		private static Player _player;
+		private const int DustRange = 1000; /* maksimalna udaljenost za dust */
+
+		#endregion
+
 		private static void Main()
 		{
+			_loaded = false;
 		}
+
+		#region pomocno
+
+		public static void PrintInfo(string text, params object[] arguments)
+		{
+			PrintEncolored(text, ConsoleColor.White, arguments);
+		}
+
+		public static void PrintSuccess(string text, params object[] arguments)
+		{
+			PrintEncolored(text, ConsoleColor.Green, arguments);
+		}
+
+		public static void PrintError(string text, params object[] arguments)
+		{
+			PrintEncolored(text, ConsoleColor.Red, arguments);
+		}
+
+		public static void PrintEncolored(string text, ConsoleColor color, params object[] arguments)
+		{
+			var _clr = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			Console.WriteLine(text, arguments);
+			Console.ForegroundColor = _clr;
+		}
+
+		#endregion
 	}
 }
