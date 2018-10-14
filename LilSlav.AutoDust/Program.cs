854c9995dbeb117add99566a238f72530833d9e9
Stefan <janjicstefan163@gmail.com>
2018-10-14 20:31:43 +0200

    Pokretanje skripte
    Provera za pokretanje skripte i ispis u konzolu ako je uspesno pokrenuta

--- LilSlav.AutoDust/Program.cs
+++ LilSlav.AutoDust/Program.cs
@@ -13,13 +13,34 @@ namespace LilSlav.AutoDust
 		private static bool _loaded;
 		private static Hero _myHero;
 		private static Player _player;
-		private const int DustRange = 1000; /* maksimalna udaljenost za dust */
+		private const int DustRange = 1000;					/* maksimalna udaljenost za dust */
+		private const string ScriptVersion = "1.0.0.1";		// za ispis
 
 		#endregion
 
 		private static void Main()
 		{
+			Game.OnUpdate += Game_OnUpdate;
 			_loaded = false;
+		}
+
+		private static void Game_OnUpdate(EventArgs args)
+		{
+			// ako je loaded false, onda ukljucujemo skriptu
+			if (!_loaded)
+			{
+				_myHero = ObjectManager.LocalHero;          // ucitamo naseg champ
+				_player = ObjectManager.LocalPlayer;
+				
+				// proveravamo da li se game ucitao
+				if (!Game.IsInGame || _myHero == null)
+				{
+					return;		// izlaz ako nismo jos usli u game
+				}
+
+				_loaded = true;
+				PrintSuccess($"> LilSlav.AutoDust Loaded v{ScriptVersion}");	// ispis u consolu ako je uspesno pokrenuta
+			}
 		}
 
 		#region pomocno
